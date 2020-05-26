ec4998dc6f65c746b0f4a7a1f27c2254e7c5a204
Bersserk <bers.developer@gmail.com>
2020-05-27 00:02:38 +0200

    new classes

---
+++ src/com/company/NewSolut.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class NewSolut {
+
+    public String myMethod(ArrayList<String> list){
+        System.out.println("some text");
+        String s = list.get(0);
+        return s;
+    }
+}
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,4 +1,63 @@
-class Solution {
+package com.company;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+import java.io.BufferedReader;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+import java.io.InputStreamReader;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+import java.util.*;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+public class Solution {
 
 
-}
+
+    public static void main(String[] args) throws Exception {
+        // Создали объект reader и список list
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> div3 = new ArrayList<>();
+        list.add("first text");
+
+        NewSolut ns = new NewSolut();
+        div3.add(ns.myMethod(list));
+
+    }
+
+    public static void someMethod (ArrayList<String> list){
+
+        System.out.println(div3);
+    }
+
+
+
+
+
+
+}                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
