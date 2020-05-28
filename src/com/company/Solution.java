9dc7a4f192829d3356f5ace0a8650c48a7b850b0
Bersserk <bers.developer@gmail.com>
2020-05-28 22:32:25 +0200

    task is done in IDE

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -4,12 +4,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 import java.util.*;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-public class Solution {                                                  
+public class Solution {
+
+    static boolean r = false;
+    static boolean l = false;
+
     public static void main(String[] args) {                                                  
         ArrayList<String> strings = new ArrayList<String>();                                                  
-        strings.add("роза");                                                  
+        strings.add("роза");
         strings.add("лоза");                                                  
-        strings.add("лира");                                                  
+        strings.add("лира");
+        strings.add("река");
+        strings.add("лара");
+        strings.add("лена");
+
         strings = fix(strings);                                                  
                                                   
         for (String string : strings) {                                                  
@@ -17,10 +25,39 @@ public class Solution {
         }                                                  
     }                                                  
                                                   
-    public static ArrayList<String> fix(ArrayList<String> strings) {                                                  
-        //напишите тут ваш код                                                  
-        return null;                                                  
-    }                                                  
+    public static ArrayList<String> fix (ArrayList<String> strings) {
+        ArrayList<String> tmp = new ArrayList<String>();
+        //напишите ту:т ваш код
+        for (int i = 0; i < strings.size(); i++ ) {
+
+            r = false;
+            l = false;
+
+            charset (strings.get(i));
+
+            if (r == true & l == false){
+
+            } else if (r == false & l == true){
+                tmp.add(strings.get(i));
+                tmp.add(strings.get(i));
+            } else {
+                tmp.add(strings.get(i));
+            }
+        }
+        return tmp;
+    }
+
+    public static void charset (String s){
+        char[] charArray = s.toCharArray();
+
+        for (int y = 0; y < charArray.length; y++){
+            if (charArray[y] == 'р'){
+                r = true;
+            } else if (charArray[y] == 'л'){
+                l = true;
+            }
+        }
+    }
 }                                                             
                                                   
                                                   
